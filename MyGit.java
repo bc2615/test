fe27476600cd558eafa0b4e36ef22fe6ea7e5722
biqing cao <biqingc@andrew.cmu.edu>
2018-01-19 15:45:07 -0500

    add date information
    date is in a good format

--- MyGit.java
+++ MyGit.java
@@ -1,8 +1,16 @@
+import java.util.*;
+import java.text.*;
 class MyGit {
-    public static void myGit() {
-        System.out.println("biqing");
-        System.out.println("I love Cloud Computing");
-    }
+	public static void myGit() {
+    Date date = new Date();
+	String your_name = "biqing";
+    SimpleDateFormat dateFormatter = new SimpleDateFormat("MMMM d, yyyy");
+    System.out.println("Date: " + dateFormatter.format(date));
+    System.out.println(your_name);
+    System.out.println("I love Cloud Computing");
+	}
+
+    
     public static void main(String[] args) {
         myGit();
     }
