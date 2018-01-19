580938620e75a4fab3ed0b17b8f7f6551cf7ff8e
biqing cao <biqingc@andrew.cmu.edu>
2018-01-19 15:56:58 -0500

    test work flow

--- MyGit.java
+++ MyGit.java
@@ -2,16 +2,9 @@ import java.util.*;
 import java.text.*;
 class MyGit {
 	public static void myGit() {
-		// today's date 
-        Date date = new Date();
-        SimpleDateFormat dateFormatter = new SimpleDateFormat("MMMM d, yyyy");
-        System.out.println("Date: " + dateFormatter.format(date));
-        // 20,000 days later
-        Calendar calendar = new GregorianCalendar();
-        calendar.add(Calendar.DATE, 20000);
-        System.out.println("After 20,000 days: " + dateFormatter.format(calendar.getTime()));
-        // your info
-        System.out.println("biqing");
+		Date date = new Date();
+		System.out.println("Date: " + date);
+		System.out.println("biqing");
         System.out.println("I love Cloud Computing");
 	}
 
