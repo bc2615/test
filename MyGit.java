742be7bdb62a067d2b82d41ebca24da2960f3222
biqing cao <biqingc@andrew.cmu.edu>
2018-01-19 15:32:48 -0500

    my first try

---
+++ MyGit.java
@@ -0,0 +1,9 @@
+class MyGit {
+    public static void myGit() {
+        System.out.println("biqing");
+        System.out.println("I love Cloud Computing");
+    }
+    public static void main(String[] args) {
+        myGit();
+    }
+}
